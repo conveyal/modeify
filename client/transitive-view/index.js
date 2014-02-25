c8f7ab1533581176042797c7a7230798edcc5475
Trevor Gerhardt <trevorgerhardt@gmail.com>
2014-02-25 11:51:38 -0500

    Replaced map with transitive instance

--- client/transitive-view/index.js
+++ client/transitive-view/index.js
@@ -2,27 +2,39 @@
  * Dependencies
  */
 
-var map = require('map');
+var config = require('config');
+var debug = require('debug')(config.name() + ':transitive-view');
+var profiler = require('otpprofiler.js');
+var Transitive = require('transitive');
 var view = require('view');
 
 /**
  * Expose `View`
  */
 
-var View = module.exports = view(require('./template.html'));
-
-/**
- * On construct
- */
-
-View.on('construct', function(view) {
-  view.on('rendered', function() {
-    var m = map(view.find('.map'), {
-      center: {
-        lat: 38.904862,
-        lng: -77.034569
-      },
-      zoom: 11
-    });
+var View = module.exports = view(require('./template.html'), function(view, model) {
+  var routes = model.routes();
+  if (routes && routes.length > 0) view.display(routes);
+  model.on('change routes', function(routes) {
+    view.display(routes);
   });
 });
+
+/**
+ * Display
+ */
+
+View.prototype.display = function(routes) {
+  debug('displaying...');
+  var el = this.find('.map');
+  var response = new profiler.models.OtpProfileResponse({
+    options: routes.slice(0, 3)
+  });
+  new profiler.transitive.TransitiveLoader(response, config.otp_url() + '/', function(data) {
+    debug('profiled and loaded');
+    var transitive = new Transitive(el, data, {}, {
+      gridCellSize: 800
+    });
+    transitive.render();
+  });
+};
