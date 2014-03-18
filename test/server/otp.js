a157204419e208aaf5719b1e235b75e10da8228e
Trevor Gerhardt <trevorgerhardt@gmail.com>
2014-03-18 15:52:04 -0400

    Beautify

--- test/server/otp.js
+++ test/server/otp.js
@@ -8,7 +8,8 @@ describe('/api/otp', function() {
   describe('GET /profile', function() {
     it('200 and return 0 options', function(done) {
       request
-        .get('/api/otp/profile' + '?from=39.76618,-86.441052&to=39.76618,-86.441052')
+        .get('/api/otp/profile' +
+          '?from=39.76618,-86.441052&to=39.76618,-86.441052')
         .expect(200)
         .end(function(err, res) {
           if (err) return done(err);
@@ -19,7 +20,9 @@ describe('/api/otp', function() {
 
     it('200 and return 3 options', function(done) {
       request
-        .get('/api/otp/profile?from=38.904862%2C-77.034569&to=38.86583312290139%2C-77.06398626875051&startTime=18%3A30&endTime=21%3A30&date=2014-03-18&orderBy=MIN&limit=3')
+        .get(
+          '/api/otp/profile?from=38.904862%2C-77.034569&to=38.86583312290139%2C-77.06398626875051&startTime=18%3A30&endTime=21%3A30&date=2014-03-18&orderBy=MIN&limit=3'
+      )
         .expect(200)
         .end(function(err, res) {
           if (err) return done(err);
