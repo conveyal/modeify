a1703dc4a358bd60a89f607ba2d3ac98e456636a
kchanba <kchanba@gmail.com>
2014-08-18 15:05:20 -0400

    transitive styling

--- client/transitive-view/style.js
+++ client/transitive-view/style.js
@@ -10,7 +10,7 @@ exports.segments = {
 // taking this value and testing other values for that property //
     switch (segment.type) {
       case 'CAR':
-        return '#000';
+        return '#510E61';
       case 'WALK':
         return 'none';
       case 'TRANSIT':
@@ -19,8 +19,65 @@ exports.segments = {
         return convert.routeToColor(route.route_type, id[0].toLowerCase(), id[1]
           .toLowerCase(), route.route_color);
     }
+  },
+
+    // override the default stroke width
+  'stroke-width': function(display, segment, index, utils) {
+    switch (segment.type) {
+      case 'CAR':
+        return utils.pixels(display.zoom.scale(), 2, 4, 6) + 'px';
+      case 'TRANSIT':
+        // bus segments:
+        if (segment.mode === 3) return utils.pixels(display.zoom.scale(), 2, 4, 8) + 'px';
+        // all others:
+        return utils.pixels(display.zoom.scale(), 4, 8, 12) + 'px';
+    }
+  },
+
+   // specify the dash-array
+  'stroke-dasharray': function(display, segment) {
+    switch (segment.type) {
+      case 'CAR':
+        return '8,3';
+    }
+  },
+
+   // specify the line cap type
+  'stroke-linecap' : function(display, segment) {
+    switch (segment.type) {
+      case 'CAR':
+        return 'butt';
+    }
+  },
+
+    // specify the circle marker for 'dotted' line styles
+  'marker-mid': function(display, segment, index, utils) {
+    var radius, fillColor;
+
+    switch(segment.type) {
+      case 'WALK':
+        radius = 3;
+        fillColor = '#5ae3f9';
+        return utils.defineSegmentCircleMarker(display, segment, radius, fillColor);
+      case 'BICYCLE':
+        radius = 2;
+        fillColor = '#f00';
+        return utils.defineSegmentCircleMarker(display, segment, radius, fillColor);
+    }
+  },
+
+  // specify the spacing for marker styling
+  'marker-spacing': function(display, segment) {
+    switch(segment.type) {
+      case 'WALK':
+        return 8;
+      case 'BICYCLE':
+        return 6;
+    }
   }
+
 };
+
 
 // start/end icons and eventually points of interest// 
 exports.places = {
@@ -28,10 +85,10 @@ exports.places = {
 };
 
 exports.places_icon = {
-  x: -8,
-  y: -8,
-  width: 16,
-  height: 16,
+  x: -16,
+  y: -16,
+  width: 32,
+  height: 32,
   'xlink:href': function(display, data) {
     if (data.owner.getId() === 'from') return config.static_url() +
       '/build/planner-app/transitive-view/start.svg';
