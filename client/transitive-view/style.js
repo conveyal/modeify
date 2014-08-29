0f297f0f2030ce2fa8c6cb985836880d89ac0cea
kchanba <kchanba@gmail.com>
2014-08-29 15:24:50 -0400

    transitive styling

--- client/transitive-view/style.js
+++ client/transitive-view/style.js
@@ -6,29 +6,33 @@ exports.places = {
   fill: 'none'
 };
 
-exports.segments = {
+var STYLES = {};
+
+STYLES.segments = {
+
+  // override the default stroke color
   stroke: function(display, segment) {
     if (!segment.focused) return;
 
-// taking this value and testing other values for that property //
     switch (segment.type) {
       case 'CAR':
-        return '#f3da01';
+        return '#888';
       case 'WALK':
-        return 'none';
-      case 'TRANSIT':
-        var route = segment.patterns[0].route;
-        var id = route.route_id.split(':');
-        return convert.routeToColor(route.route_type, id[0].toLowerCase(), id[1]
-          .toLowerCase(), route.route_color);
+        return '#5492C0';
+      case 'BICYCLE':
+        return '#f00';
+
     }
   },
 
-    // override the default stroke width
+  // override the default stroke width
   'stroke-width': function(display, segment, index, utils) {
     switch (segment.type) {
       case 'CAR':
         return utils.pixels(display.zoom.scale(), 2, 4, 6) + 'px';
+      case 'WALK':
+      case 'BICYCLE':
+        return '4px'
       case 'TRANSIT':
         // bus segments:
         if (segment.mode === 3) return utils.pixels(display.zoom.scale(), 2, 4, 8) + 'px';
@@ -37,25 +41,30 @@ exports.segments = {
     }
   },
 
-   // specify the dash-array
+  // specify the dash-array
   'stroke-dasharray': function(display, segment) {
     switch (segment.type) {
       case 'CAR':
-        return '8,3';
+        return '3,2';
+      case 'WALK':
+      case 'BICYCLE':
+        return '0.01,6';
     }
   },
 
-   // specify the line cap type
+  // specify the line cap type
   'stroke-linecap' : function(display, segment) {
     switch (segment.type) {
       case 'CAR':
         return 'butt';
+      case 'WALK':
+      case 'BICYCLE':
+        return 'round';
     }
   },
 
-
   // specify the circle marker for 'dotted' line styles
-  'marker-mid': function(display, segment, index, utils) {
+  /*'marker-mid': function(display, segment, index, utils) {
     var radius, fillColor;
 
     switch(segment.type) {
@@ -64,7 +73,7 @@ exports.segments = {
         fillColor = '#5ae3f9';
         return utils.defineSegmentCircleMarker(display, segment, radius, fillColor);
       case 'BICYCLE':
-        radius = 3;
+        radius = 2;
         fillColor = '#f00';
         return utils.defineSegmentCircleMarker(display, segment, radius, fillColor);
     }
@@ -74,10 +83,24 @@ exports.segments = {
   'marker-spacing': function(display, segment) {
     switch(segment.type) {
       case 'WALK':
-        return 10;
+        return 8;
       case 'BICYCLE':
-        return 10;
+        return 6;
     }
+  }*/
+
+};
+
+
+/** style overrides for segment-based labels **/
+
+STYLES.segment_label_containers = {
+
+  // specify the fill color for the label bubble
+  fill: function(display, label) {
+    if(!label.isFocused()) return;
+
+    return '#008';
   }
 };
 
