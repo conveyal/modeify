1f68d4d227a32dcefd4f1d6d5822d0591bdd6098
Trevor Gerhardt <trevorgerhardt@gmail.com>
2014-09-02 18:03:14 -0400

    Remove window.alert from feedback modal

--- client/alert/index.js
+++ client/alert/index.js
@@ -1,17 +1,10 @@
-/**
- * Dependencies
- */
-
-var template = require('./template.html');
 var create = require('view');
 
 /**
  * Expose `Alert`
  */
 
-var Alert = module.exports = create(template, function(alert) {
-  document.getElementById('alerts').appendChild(alert.el);
-});
+var Alert = module.exports = create(require('./template.html'));
 
 /**
  * Dispose
--- client/feedback-modal/index.js
+++ client/feedback-modal/index.js
@@ -1,4 +1,5 @@
 var analytics = require('analytics');
+var Alert = require('alert');
 var config = require('config');
 var debug = require('debug')(config.application() + ':feedback-modal');
 var modal = require('modal');
@@ -19,10 +20,18 @@ var Modal = module.exports = modal({
 
 Modal.prototype.submit = function(e) {
   e.preventDefault();
+  var alerts = this.find('.alerts');
+  var button = this.find('button');
   var feedback = this.find('textarea').value + '';
+
   if (!feedback || feedback.length < 1) {
-    window.alert('Please fill in the feedback field above.');
+    alerts.appendChild(Alert({
+      type: 'warning',
+      text: 'Please fill in the feedback field below.'
+    }).el);
   } else {
+    button.remove();
+
     var plan = session.plan().toJSON();
 
     delete plan.options;
@@ -34,7 +43,14 @@ Modal.prototype.submit = function(e) {
       plan: plan
     });
 
-    window.alert('Thanks! We appreciate the feedback!');
-    this.hide();
+    alerts.appendChild(Alert({
+      type: 'success',
+      text: 'Thanks! We appreciate the feedback!'
+    }).el);
+
+    var self = this;
+    setTimeout(function() {
+      self.hide();
+    }, 2500);
   }
 };
