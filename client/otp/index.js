b827d06b0f66b7e75f256b6beff32fe33e920760
Trevor Gerhardt <trevorgerhardt@gmail.com>
2014-09-04 16:02:24 -0400

    Log URL to get raw results

--- client/otp/index.js
+++ client/otp/index.js
@@ -1,6 +1,7 @@
 var config = require('config');
-var debug = require('debug')(config.application() + ':otp');
+var log = require('log')('otp');
 var Profiler = require('otp-profiler');
+var qs = require('querystring');
 
 /**
  * Create profiler
@@ -15,19 +16,30 @@ var profiler = new Profiler({
  */
 
 module.exports = function profile(query, callback) {
-  debug('--> profiling %s', JSON.stringify(query));
+  log.info('--> profiling');
+  log.info('--  see raw results here: %s/api/otp/profile?%s', config.base_url(), decodeURIComponent(qs.stringify({
+    from: query.from.lat + ',' + query.from.lon,
+    to: query.to.lat + ',' + query.to.lon,
+    bikeSpeed: query.bikeSpeed,
+    startTime: query.startTime,
+    endTime: query.endTime,
+    date: query.date,
+    modes: query.modes,
+    walkSpeed: query.walkSpeed
+  })));
+
   profiler.profile(query, function(err, data) {
     if (err || !data) {
-      debug('<-- error profiling', err);
+      log.error('<-- error profiling %j', err);
       callback(err);
     } else {
       query.profile = data;
       profiler.journey(query, function(err, journey) {
         if (err) {
-          debug('<-- error profiling', err);
+          log.error('<-- error profiling %j', err);
           callback(err);
         } else {
-          debug('<-- profiled %s options', data.options.length);
+          log.info('<-- profiled %s options', data.options.length);
           callback(null, {
             journey: journey,
             options: data.options
