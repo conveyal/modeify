be0759b49b3dd08071fec980de59ad64153471ba
Trevor Gerhardt <trevorgerhardt@gmail.com>
2014-09-22 15:14:40 -0400

    Default port to 80

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var api = require('./lib/api');
 var http = require('http');
 var log = require('./lib/log');
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80;
 
 // Create and run the server
 
