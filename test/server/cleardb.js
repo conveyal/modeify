3b4784c78f1d95af689b46818f80aa67f0db2b31
Trevor Gerhardt <trevorgerhardt@gmail.com>
2014-10-07 14:47:35 -0400

    Fix cleardb bug

--- test/server/cleardb.js
+++ test/server/cleardb.js
@@ -3,7 +3,8 @@ var mongoose = require('../../lib/mongo');
 
 module.exports = function(callback) {
   if (process.env.NODE_ENV !== 'test') return callback();
-  async.each([ 'commuters', 'organizations', 'users' ], function(collection, done) {
-    mongoose.connection.collections[collection].drop(done);
+  async.each([ 'commuters', 'organizations', 'users' ], function(name, done) {
+    var collection = mongoose.connection.collections[name];
+    if (collection) collection.drop(done);
   }, callback);
 };
