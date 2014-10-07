451436f66669ccd758886520344e67f59486a975
Trevor Gerhardt <trevorgerhardt@gmail.com>
2014-10-07 14:58:38 -0400

    Remove all instead of drop

--- test/server/cleardb.js
+++ test/server/cleardb.js
@@ -3,8 +3,7 @@ var mongoose = require('../../lib/mongo');
 
 module.exports = function(callback) {
   if (process.env.NODE_ENV !== 'test') return callback();
-  async.each([ 'commuters', 'organizations', 'users' ], function(name, done) {
-    var collection = mongoose.connection.collections[name];
-    if (collection) collection.drop(done);
+  async.each([ 'Commuter', 'Organization', 'User' ], function(name, done) {
+    mongoose.model(name).remove({}, done);
   }, callback);
 };
