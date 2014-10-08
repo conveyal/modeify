7b169bcba27daad0aa22db65bf1baa28df7f2ce0
Trevor Gerhardt <trevorgerhardt@gmail.com>
2014-10-08 15:18:51 -0400

    Use config vars for survey link and mailto link

--- client/beta-bar/index.js
+++ client/beta-bar/index.js
@@ -1,16 +1,14 @@
-// var config = require('config');
+var config = require('config');
 var view = require('view');
 
 var View = module.exports = view(require('./template.html'));
 
 View.prototype.survey = function() {
-  return '';
-  /*return config.survey
+  return config.survey
     ? config.survey()
-    : ''; */
+    : '';
 };
 
 View.prototype.email = function() {
-  return '';
-  // return config.email().address;
+  return 'mailto:' + config.email().address;
 };
