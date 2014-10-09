55fdd4e919a9097e7d046a507737882277b65a64
Trevor Gerhardt <trevorgerhardt@gmail.com>
2014-10-09 13:33:46 -0400

    Add drag and drop

--- client/geocode/index.js
+++ client/geocode/index.js
@@ -6,6 +6,7 @@ var get = require('request').get;
  */
 
 module.exports = geocode;
+module.exports.reverse = reverse;
 module.exports.suggest = suggest;
 
 /**
@@ -17,6 +18,23 @@ function geocode(address, callback) {
   get('/geocode/' + address, function(err, res) {
     if (err) {
       log('<-- geocoding error %s', err);
+      callback(err, res);
+    } else {
+      log('<-- geocoding complete %j', res.body);
+      callback(null, res.body);
+    }
+  });
+}
+
+/**
+ * Reverse geocode
+ */
+
+function reverse(ll, callback) {
+  log('--> reverse geocoding %s', ll);
+  get('/geocode/reverse/' + ll[0] + ',' + ll[1], function(err, res) {
+    if (err) {
+      log('<-- geocoding error %e', err);
       callback(err, res);
     } else {
       log('<-- geocoding complete %j', res.body);
