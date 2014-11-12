d729df7d0cc500d3438d397eb4950f9d8b5d4ab4
Trevor Gerhardt <trevorgerhardt@gmail.com>
2014-11-12 08:07:50 -0500

    Back to old scrollbar adjustment

--- client/modal/index.js
+++ client/modal/index.js
@@ -47,6 +47,7 @@ module.exports = function(opts, fn) {
       // Set the scrollbar size
       var div = el.querySelector('div');
       div.style.marginRight = -scrollbarSize + 'px';
+      div.style.paddingRight = scrollbarSize + 'px';
 
       setTimeout(function() {
         var height = el.clientHeight;
