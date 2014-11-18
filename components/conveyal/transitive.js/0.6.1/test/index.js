0f73984ba911c56be4ebf5f2daeb04cbd1accf8c
Trevor Gerhardt <trevorgerhardt@gmail.com>
2014-11-17 23:33:38 -0500

    Updated components

---
+++ components/conveyal/transitive.js/0.6.1/test/index.js
@@ -0,0 +1,62 @@
+/**
+ * Dependencies
+ */
+
+var d3 = require('d3');
+
+var Transitive = require('transitive');
+var OtpProfiler = require('otpprofiler.js');
+
+var transitive = new Transitive({
+  el: document.getElementById('canvas'),
+  legendEl: document.getElementById('legend'),
+  data: DATA,
+  styles: STYLES,
+  gridCellSize: 300,
+  useDynamicRendering: true,
+  drawGrid: false,
+  mapboxId: 'conveyal.ie3o67m0',
+  initialBounds: [
+    [-77.093507, 38.921104],
+    [-76.947266, 38.858710]
+  ],
+  displayMargins: {
+    right: 400,
+    bottom: 50
+  },
+  draggableTypes: ['PLACE']
+});
+
+// listen for place drag events
+transitive.on('place.from.dragend', function(place) {
+  console.log('dragged "from" place to: ' + place.getLat() + ',' + place.getLon());
+});
+transitive.on('place.to.dragend', function(place) {
+  console.log('dragged "to" place to: ' + place.getLat() + ',' + place.getLon());
+});
+
+// apply computed behaviors
+transitive.on('render', function(transitive) {
+  each(COMPUTED, function(behavior) {
+    behavior(transitive);
+  });
+});
+
+transitive.render();
+
+// set the journey option list
+DATA.journeys.forEach(function(journey, index) {
+  var div = document.createElement("div");
+  div.id = journey.journey_id;
+  div.className = 'listItem';
+  div.innerHTML = journey.journey_name;
+
+  div.onmouseover = function(event) {
+    transitive.focusJourney(event.target.id);
+  };
+  div.onmouseout = function(event) {
+    transitive.focusJourney();
+
+  };
+  document.getElementById('list').appendChild(div);
+});
---
+++ components/yields/has-transitions/0.0.1/index.js
@@ -0,0 +1,46 @@
+
+/**
+ * Check if `el` or browser supports transitions.
+ *
+ * @param {Element} el
+ * @return {Boolean}
+ * @api public
+ */
+
+exports = module.exports = function(el){
+  switch (arguments.length) {
+    case 0: return bool;
+    case 1: return bool
+      ? transitions(el)
+      : bool;
+  }
+};
+
+/**
+ * Check if the given `el` has transitions.
+ *
+ * @param {Element} el
+ * @return {Boolean}
+ * @api private
+ */
+
+function transitions(el, styl){
+  if (el.transition) return true;
+  styl = window.getComputedStyle(el);
+  return !! styl.transition;
+}
+
+/**
+ * Style.
+ */
+
+var styl = document.body.style;
+
+/**
+ * Export support.
+ */
+
+var bool = 'transition' in styl
+  || 'webkitTransition' in styl
+  || 'MozTransition' in styl
+  || 'msTransition' in styl;
