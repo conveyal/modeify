d28835c4633391e523bfdecfbfcd09ab5118e332
Trevor Gerhardt <trevorgerhardt@gmail.com>
2014-12-15 16:01:05 -0500

    Pass the response body back only

--- client/route-resource/index.js
+++ client/route-resource/index.js
@@ -5,5 +5,11 @@ exports.findByTags = function(tags, callback) {
   log('finding resources that match %j', tags);
   request.get('/route-resources', {
     tags: tags.join(',')
-  }, callback);
+  }, function(err, res) {
+    if (err || !res.body) {
+      callback('No resources found.');
+    } else {
+      callback(null, res.body);
+    }
+  });
 };
