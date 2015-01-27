abfbb19beaf39f7466a4797217d68a8c62312666
Trevor Gerhardt <trevorgerhardt@gmail.com>
2015-01-27 09:59:25 -0500

    Fix broken test

--- test/server/otp.js
+++ test/server/otp.js
@@ -13,7 +13,7 @@ describe('/api/otp', function() {
         .expect(400, done);
     });
 
-    it('200 and return 11 options', function(done) {
+    it('200 and return 12 options', function(done) {
       request
         .get(
           '/api/otp/profile?from=38.86583312290139%2C-77.06398626875051&to=38.90485941802882%2C-77.03453592419277'
@@ -21,7 +21,7 @@ describe('/api/otp', function() {
         .expect(200)
         .end(function(err, res) {
           if (err) return done(err);
-          res.body.options.length.should.equal(11);
+          res.body.options.length.should.equal(12);
           done();
         });
     });
