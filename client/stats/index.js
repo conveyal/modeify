28d8c75b8c19f7b76adcea660c46c4a6b58b0fc1
Trevor Gerhardt <trevorgerhardt@gmail.com>
2015-02-12 17:56:31 -0500

    Add stats in dev mode

---
+++ client/stats/index.js
@@ -0,0 +1,33 @@
+var MemoryStats = require('./memory-stats');
+var Stats = require('./stats');
+
+var stats = module.exports = new Stats();
+var memory = module.exports.memory = new MemoryStats();
+
+memory.domElement.style.position = 'fixed';
+memory.domElement.style.left = '0px';
+memory.domElement.style.bottom = '0px';
+
+stats.domElement.style.position = 'fixed';
+stats.domElement.style.left = '80px';
+stats.domElement.style.bottom = '0px';
+
+document.body.appendChild(memory.domElement);
+document.body.appendChild(stats.domElement);
+
+requestAnimationFrame(function rAFloop() {
+  // By default measure between animation frames
+  stats.update();
+  memory.update();
+  requestAnimationFrame(rAFloop);
+});
+
+module.exports.show = function() {
+  memory.domElement.style.display = 'block';
+  stats.domElement.style.display = 'block';
+};
+
+module.exports.hide = function() {
+  memory.domElement.style.display = 'none';
+  stats.domElement.style.display = 'none';
+};
