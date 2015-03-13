33c8592d0385d3cccc11609c42f29bf7491d2266
Trevor Gerhardt <trevorgerhardt@gmail.com>
2015-03-13 18:57:01 -0400

    Pull out scrollbar-size into it's own component

--- components/trevorgerhardt/scrollbar-size/master/index.js
+++ components/trevorgerhardt/scrollbar-size/master/index.js
@@ -1,17 +1,17 @@
-var outer = document.createElement("div");
-outer.style.visibility = "hidden";
-outer.style.width = "100px";
-outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+var outer = document.createElement('div');
+outer.style.visibility = 'hidden';
+outer.style.width = '100px';
+outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
 
 document.body.appendChild(outer);
 
 var widthNoScroll = outer.offsetWidth;
 // force scrollbars
-outer.style.overflow = "scroll";
+outer.style.overflow = 'scroll';
 
 // add innerdiv
-var inner = document.createElement("div");
-inner.style.width = "100%";
+var inner = document.createElement('div');
+inner.style.width = '100%';
 outer.appendChild(inner);
 
 var widthWithScroll = inner.offsetWidth;
