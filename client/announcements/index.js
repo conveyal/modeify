b7d9fe49088c4ac37f3fba192d142c219ecc3b91
Trevor Gerhardt <trevorgerhardt@gmail.com>
2015-03-23 18:44:41 -0400

    Show announcements every time for now

--- client/announcements/index.js
+++ client/announcements/index.js
@@ -21,7 +21,7 @@ module.exports = function showAnnouncements(ctx, next) {
   var profile = session.commuter().profile();
 
   if (profile.welcome_wizard_complete) {
-    var unnseen = getUnseenAnnouncements(profile.announcements_seen || []);
+    var unnseen = getUnseenAnnouncements([]);
     if (unnseen.length > 0) {
       log('welcome complete, showing announcements');
       modal = new Modal({
