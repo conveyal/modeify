c19d8d6244af282feb45a782d5213b31c845efbe
Victor Chernetsky <victor@amigocloud.com>
2015-05-28 16:47:41 +0000

    Added support for google analytics - Just need to set your GA keys in deployment/config.yaml

--- client/analytics/index.js
+++ client/analytics/index.js
@@ -15,3 +15,10 @@ module.exports.track = function(name, data) {
   if (config.segmentio_key())
     window.analytics.track(name, tableize(data || {}));
 };
+
+module.exports.send_ga = function (e) {
+  var ignore = location.host.indexOf(config.ignore_events_from()) !== -1;
+  if (!ignore && config.ga_key && config.ga_key()) {
+    ga('send', 'event', e.category, e.action, e.label, e.value);
+  }
+};
