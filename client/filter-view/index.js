89ac005ffd61d780780b28d8c153c49defe68468
kchanba <kchanba@gmail.com>
2015-09-24 11:35:44 -0400

    First pass bike speeds

--- client/filter-view/index.js
+++ client/filter-view/index.js
@@ -62,6 +62,14 @@ View.prototype.hideSettings = function () {
   this.find('.settings-form').classList.remove('settings-form-open')
 }
 
+var bikeSelect = this.find('bike-settings select');
+
+var bikeSpeed = bikeSelect.value;
+
+View.prototype.updateBikeSpeed = function () {
+  console.log("bikeSelect");
+  console.log("bikeSpeed");
+}
 
 
 
