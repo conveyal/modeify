6fa286faa6ccea2428987f3ee3e7988cf9de6be0
David Emory <demory@conveyal.com>
2016-02-10 09:07:40 -0500

    Wire up copy links buttons in share dialog

--- client/share-view/index.js
+++ client/share-view/index.js
@@ -1,12 +1,38 @@
 var modal = require('./client/modal')
+var session = require('session')
 
 
 /**
  * Expose `Modal`
  */
 
-var Modal = module.exports = modal({
+var ShareModal = module.exports = modal({
   closable: true,
   width: '768px',
   template: require('./template.html')
-}, function (view, routes) {})
+}, function (view, routes) {
+
+  view.find('.trip-link').value = window.location.href;
+  view.find('.place-link').value = constructPlaceLink(session.plan().from(), 'from');
+
+  var tripClipboard = new Clipboard('#copy-trip-btn');
+  tripClipboard.on('success', function(e) {
+      e.clearSelection();
+  });
+
+  var placeClipboard = new Clipboard('#copy-place-btn');
+  placeClipboard.on('success', function(e) {
+      e.clearSelection();
+  });
+})
+
+ShareModal.prototype.placeChanged = function (e) {
+  var val = this.find('.place-select').value;
+  var place = session.plan().get(val);
+  this.find('.place-link').value = constructPlaceLink(place, val);
+}
+
+function constructPlaceLink(place, type) {
+  var loc = window.location;
+  return loc.protocol + "//" + loc.hostname + (loc.port ? ':' + loc.port: '') + '/?' + (type === 'from' ? 'planFrom' : 'planTo') + '=' + encodeURIComponent(place);
+}
