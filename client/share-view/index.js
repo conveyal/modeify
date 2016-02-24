3554a6aa1993bdb3481e0122211c702656a8ffb9
David Emory <demory@conveyal.com>
2016-02-24 09:49:49 -0500

    Email shared trip

--- client/share-view/index.js
+++ client/share-view/index.js
@@ -1,6 +1,7 @@
 var modal = require('./client/modal')
 var session = require('session')
-
+var request = require('request')
+var Alert = require('alert')
 
 /**
  * Expose `Modal`
@@ -26,6 +27,29 @@ var ShareModal = module.exports = modal({
   });
 })
 
+ShareModal.prototype.sendEmail = function () {
+  var self = this
+
+  request.post('/share/share-trip', {
+    from: this.find('.from-email').value,
+    to: this.find('.to-email').value,
+    subject: this.find('.email-subject').value,
+    body: this.find('.email-body').value,
+    link: window.location.href
+  }, function (
+    err, res) {
+    if (err || !res.ok) {
+      console.error('error sharing trip', err)
+    } else {
+      console.log('shared!, showing alert')
+      self.find('.alerts').appendChild(Alert({
+        type: 'success',
+        text: 'Your trip has been sent to ' + self.find('.to-email').value + '!'
+      }).el)
+    }
+  })
+}
+
 ShareModal.prototype.placeChanged = function (e) {
   var val = this.find('.place-select').value;
   var place = session.plan().get(val);
@@ -36,3 +60,4 @@ function constructPlaceLink(place, type) {
   var loc = window.location;
   return loc.protocol + "//" + loc.hostname + (loc.port ? ':' + loc.port: '') + '/?' + (type === 'from' ? 'planFrom' : 'planTo') + '=' + encodeURIComponent(place);
 }
+
