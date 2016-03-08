5525261a7f07fecaae19309cb7e1c3ec40fd688e
Trevor Gerhardt <trevorgerhardt@gmail.com>
2016-03-08 11:24:55 -0300

    Update some way out of date packages

--- babelify.js
+++ babelify.js
@@ -1,3 +1,5 @@
-require('babel/register')({
-  ignore: /node_modules(?!\/commuter-connections)/
+require('babel-register')({
+  ignore: /node_modules(?!\/commuter-connections)/,
+  plugins: ['add-module-exports'],
+  presets: ['es2015', 'stage-0']
 })
