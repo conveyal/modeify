986590af3f75bb71fcd47eed29f7f6854c0eac91
Trevor Gerhardt <trevorgerhardt@gmail.com>
2016-03-10 17:31:08 -0300

    More CSS configurations

--- client/analytics/index.js
+++ client/analytics/index.js
@@ -2,19 +2,19 @@ var config = require('./client/config')
 var tableize = require('tableize')
 
 module.exports.identify = function (id, data) {
-  if (config.segmentio_key()) {
+  if (config.segmentio_key() && window.analytics) {
     window.analytics.identify(id, tableize(data || {}))
   }
 }
 
 module.exports.page = function (name, category) {
-  if (config.segmentio_key()) {
+  if (config.segmentio_key() && window.analytics) {
     window.analytics.page(name, category)
   }
 }
 
 module.exports.track = function (name, data) {
-  if (config.segmentio_key()) {
+  if (config.segmentio_key() && window.analytics) {
     window.analytics.track(name, tableize(data || {}))
   }
 }
