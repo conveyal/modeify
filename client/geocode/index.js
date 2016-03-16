2d9283f7f1801b2de3e787a44591c210b4f7b71d
jonathan <Furystrikee123>
2016-03-16 15:52:20 -0500

    adding request

--- client/geocode/index.js
+++ client/geocode/index.js
@@ -117,3 +117,12 @@ function suggest(text, callback) {
   });
 
 }
+
+var request = require('request');
+request('https://www.amigocloud.com/api/v1/me/geocoder/autocomplete?text=palo&token=R:3jqO9zmsFuFpdn0BosPJbXpjf82PISOJXqMbwN', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log("body", body);
+    console.log("response",response);
+    console.log("error", error)
+  }
+});
