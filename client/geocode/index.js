d18c56e8890e630ac943388da654b1e72dbeff8a
jonathan <Furystrikee123>
2016-03-16 16:19:33 -0500

    adding changes request part2

--- client/geocode/index.js
+++ client/geocode/index.js
@@ -117,11 +117,6 @@ function suggest(text, callback) {
 
 }
 
-var request = require('request');
-request('https://www.amigocloud.com/api/v1/me/geocoder/autocomplete?text=palo&token=R:3jqO9zmsFuFpdn0BosPJbXpjf82PISOJXqMbwN', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log("body", body);
-    console.log("response",response);
-    console.log("error", error)
-  }
-});
+
+var test = require('../../components/request_modeify');
+console.log("test", test);
