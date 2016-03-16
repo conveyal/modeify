52400102e5433937ff13877cd7dc94ad142d059e
jonathan <Furystrikee123>
2016-03-16 16:42:45 -0500

    adding changes request

--- components/request_modeify/index.js
+++ components/request_modeify/index.js
@@ -1,10 +1,21 @@
-var request = require('request');
+var request = require('fetchy-request');
+var request1 = require('fetchy-request');
 
-request('https://www.amigocloud.com/api/v1/me/geocoder/autocomplete?text=palo&token=R:3jqO9zmsFuFpdn0BosPJbXpjf82PISOJXqMbwN', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log("body", body);
-    console.log("response",response);
-    console.log("error", error)
-  }
+
+request('https://www.amigocloud.com/api/v1/me/geocoder/autocomplete?text=palo&token=R:3jqO9zmsFuFpdn0BosPJbXpjf82PISOJXqMbwN')
+    .then(function (response) {
+        return response.json()
+    }).then(function (json) {
+    console.log('parsed json', json)
+});
+
+
+request1({
+    uri: 'https://www.amigocloud.com/api/v1/me/geocoder/autocomplete',
+    method: 'GET',
+    qs: {
+        text1: 'palo',
+        token: 'R:3jqO9zmsFuFpdn0BosPJbXpjf82PISOJXqMbwN'
+    }
 });
 
