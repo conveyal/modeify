04251f10390fc3402dc37633796ad68d2fd71e99
jonathan <Furystrikee123>
2016-03-21 10:25:01 -0500

    console reserver

--- client/geocode/index.js
+++ client/geocode/index.js
@@ -54,8 +54,9 @@ function reverse(ll, callback) {
 
 function reverseAmigo(ll, callback) {
   log('--> reverse geocoding %s', ll);
-
-  var parameter = {'point.lon':ll[0], 'point.lat':ll[1], 'token':'R:DNiePlGOMsw93cEgde88woWAQxm1xzWt7lvVXe' };
+  console.log("object ll", ll);
+  console.log("object ll type", typeof(ll));
+  var parameter = {'token':'R:DNiePlGOMsw93cEgde88woWAQxm1xzWt7lvVXe' , 'point.lon':ll[0], 'point.lat':ll[1] };
   get('https://www.amigocloud.com/api/v1/me/geocoder/reverse', parameter, function(err, res) {
     console.log("change reverse", res);
     if (err) {
