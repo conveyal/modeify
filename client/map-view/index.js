ea37f87ffb749c25f8704303da96c065b041264b
Jose Ramirez <jcramireztello@gmail.com>
2016-03-28 10:31:09 -0500

    grafica

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -63,28 +63,7 @@ module.exports = function(el) {
     map.realtimeControl = L.control.toggleRealTime().addTo(map);
 
 
-    var drawRoute = function (route) {
 
-      route = new L.Polyline(L.PolylineUtil.decode(route, 5));
-
-      console.log("route ->",route);
-
-      var boxes = L.RouteBoxer.box(route, 5);
-      var bounds = new L.LatLngBounds([]);
-      var boxpolys = new Array(boxes.length);
-
-      for (var i = 0; i < boxes.length; i++) {
-        //L.rectangle(boxes[i], {color: "#ff7800", weight: 1}).addTo(this.map);
-        bounds.extend(boxes[i]);
-      }
-
-      route.addTo(map);
-      map.fitBounds(bounds);
-
-      return route;
-
-
-    };
 
 
     /*
@@ -115,16 +94,39 @@ module.exports.getMap = function () {
   return this.activeMap;
 };
 
+var drawRoute = function (route) {
+
+      route = new L.Polyline(L.PolylineUtil.decode(route, 5));
+
+      console.log("route ->",route);
+
+      var boxes = L.RouteBoxer.box(route, 5);
+      var bounds = new L.LatLngBounds([]);
+      var boxpolys = new Array(boxes.length);
+
+      for (var i = 0; i < boxes.length; i++) {
+        //L.rectangle(boxes[i], {color: "#ff7800", weight: 1}).addTo(this.map);
+        bounds.extend(boxes[i]);
+      }
+
+      route.addTo(this.activeMap);
+      this.activeMap.fitBounds(bounds);
+
+
+    };
+
 module.exports.drawRoutePlan = function(dataplan) {
     var itineraries = dataplan.plan.itineraries;
     for (i = 0; i < itineraries.length; i++) {
         for (ii=0; ii < itineraries[i].legs.length; ii++) {
 
                 console.log("jalan los codigos",  itineraries[i].legs[ii].legGeometry.points);
-                drawRouteAmigo(itineraries[i].legs[ii].legGeometry.points);
+                drawRoute(itineraries[i].legs[ii].legGeometry.points);
         }
     }
 }
+
+
 
 module.exports.drawRouteAmigo = function(route) {
     route = new L.Polyline(L.PolylineUtil.decode(route, 5));
