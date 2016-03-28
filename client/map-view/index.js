2d9a2b35b02494c16c88316f06d256f4f50c5956
Jose Ramirez <jcramireztello@gmail.com>
2016-03-28 18:39:18 -0500

    cambio de color

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -111,15 +111,13 @@ module.exports.cleanRoute = function() {
 };
 
 module.exports.drawRouteAmigo = function(route) {
-      route = new L.Polyline(L.PolylineUtil.decode(route, 5));
-        /*
+      //route = new L.Polyline(L.PolylineUtil.decode(route, 5));
+
+       console.log(L.PolylineUtil.decode(route, 5));
       route = new L.Polyline({
             path: L.PolylineUtil.decode(route, 5),
-            geodesic: true,
-            strokeColor: '#FF0000',
-            strokeOpacity: 1.0,
-            strokeWeight: 2
-        });*/
+            strokeColor: '#FF0000'
+        });
 
         console.log("cambio metodos route");
       var boxes = L.RouteBoxer.box(route, 5);
