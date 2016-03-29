f0d4903966cf70532b7845de130c2b6fadf0bdc8
jonathan <Furystrikee123>
2016-03-29 18:05:33 -0500

    adding html layer

--- client/planner-page/index.js
+++ client/planner-page/index.js
@@ -389,6 +389,7 @@ function updateMapOnPlanChange(plan, map) {
             showMapView.marker_map([plan.dataplan.from.lat,plan.dataplan.from.lon],[plan.dataplan.to.lat,plan.dataplan.to.lon], map);
             for (i = 0; i < itineraries.length; i++) {
                 for (ii=0; ii < itineraries[i].legs.length; ii++) {
+                  console.log("itineraries[i].legs[ii]", itineraries[i].legs[ii]);
                   showMapView.drawRouteAmigo(itineraries[i].legs[ii].legGeometry.points, itineraries[i].legs[ii].mode);
                 }
             }
