64c400e204c8b328990f457e9974029b91c3a4fb
jonathan <Furystrikee123>
2016-03-30 16:49:27 -0500

    adding console

--- client/plan/update-routes.js
+++ client/plan/update-routes.js
@@ -159,16 +159,16 @@ function updateRoutes(plan, opts, callback) {
     console.log("populateSegments0");
 	return;
 
-      // Get the car data
-      //var driveOption = new Route(data.options.filter(function(o) {
-      //  return o.access[0].mode === 'CAR' && (!o.transit || o.transit.length < 1);
-      //})[0]);
+       //Get the car data
+      var driveOption = new Route(data.options.filter(function(o) {
+        return o.access[0].mode === 'CAR' && (!o.transit || o.transit.length < 1);
+      })[0]);
 
       // Remove the car option if car is turned off
       if (!plan.car()) {
-        //data.options = data.options.filter(function(o) {
-        //  return o.access[0].mode !== 'CAR';
-        //});
+        data.options = data.options.filter(function(o) {
+          return o.access[0].mode !== 'CAR';
+        });
 
         data.journey.journeys = data.journey.journeys.filter(function(o) {
           return o.journey_name.indexOf('CAR') === -1;
