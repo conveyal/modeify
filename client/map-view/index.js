50bf82834ed0574de6bc524fb1123a7540585c49
jonathan <Furystrikee123>
2016-03-31 10:06:40 -0500

    adding console

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -186,7 +186,7 @@ module.exports.marker_map_point = function(to, map){
         weight: 2,
         fillColor: '#ffffff',
         fillOpacity: 1
-    }).setPopupContent(name).addTo(map);
+    }).bindPopup(name).addTo(map);
 
 
     var myZoom = {
