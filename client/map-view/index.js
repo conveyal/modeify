b4d70b5ad40c85690ea85e95341d2af51c6ca53c
jonathan <Furystrikee123>
2016-03-31 11:50:29 -0500

    adding console

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -148,7 +148,7 @@ module.exports.marker_map = function(from, to, map){
 
     //L.marker([37.35337508231001,-121.93626880645752], {icon: IconStart}).bindPopup('From').addTo(map);
     //L.marker([37.44377324953697,-122.16601610183714], {icon: IconEnd}).bindPopup('to').addTo(map);
-    var markerform = new L.marker([from[0],from[1]], {icon: IconStart, draggable: true}).bindPopup('From').addTo(map);
+    var markerform = new L.marker([from[0],from[1]], {icon: IconStart, draggable: true}).bindLabel('From').addTo(map);
     var markerto = new L.marker([to[0],to[1]], {icon: IconEnd, draggable: true}).bindPopup('to').addTo(map);
     var _this = this;
     markerform.on('dragend', function(e){
