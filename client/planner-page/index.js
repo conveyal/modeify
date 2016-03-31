dc3ff47b154f86279ce453f5cb5db7912a313896
jonathan <Furystrikee123>
2016-03-31 10:11:46 -0500

    adding console

--- client/planner-page/index.js
+++ client/planner-page/index.js
@@ -417,7 +417,7 @@ function updateMapOnPlanChange(plan, map) {
             for (var n = 0; n < routes.length; n++) {
 
                  for (var m=0; m < routes[n].length; m++) {
-                     console.log("routes[m].shortName", routes[n][m].shortName);
+                     console.log("routes[m].shortName", routes[n])
                   var route_short_name = routes[m].shortName;
                   var route_long_name = routes[m].longName.toUpperCase();
                   console.log("route_short_name ->", route_short_name);
