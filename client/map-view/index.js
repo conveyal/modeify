0536c3b5542273d6efb6ce8c106056686b7f6144
jonathan <Furystrikee123>
2016-04-01 14:46:29 -0500

    adding map zoom

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -211,6 +211,26 @@ module.exports.marker_map_point = function(to, map){
 
     var layer = L.layerGroup(markers).addTo(map);
 
+     var myZoom = {
+      start:  map.getZoom(),
+      end: map.getZoom()
+    };
+    map.on('zoomstart', function(e) {
+       myZoom.start = map.getZoom();
+        console.log("start zoom 400 ", myZoom.start);
+    });
+    map.on('zoomend', function(e) {
+        myZoom.end = map.getZoom();
+         //console.log("End zoom ", myZoom.end);
+        var diff = myZoom.start - myZoom.end;
+        if (diff > 0) {
+            circle.setRadius(circle.getRadius() * 2);
+        } else if (diff < 0) {
+            circle.setRadius(circle.getRadius() / 2);
+        }
+    });
+
+
     this.makerpoint_creadas.push(layer);
     //this.polyline_creadas.push(circle);
 };
