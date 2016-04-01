b3eb2502d8553103427aac8ee98035e87637c5ee
jonathan <Furystrikee123>
2016-04-01 16:18:11 -0500

    change maps

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -206,7 +206,7 @@ module.exports.marker_map_point = function(to, map){
         popupAnchor:  [-3, -76]
     });
     var markers = [
-      L.marker([to[0], to[1]], {icon: IconEnd}).bindLabel(name, { direction: 'auto' })
+      L.marker([to[0], to[1]], {icon: IconEnd}).bindLabel(name, { direction: 'auto', noHide: true })
     ];
 
     var layer = L.layerGroup(markers).addTo(map).eachLayer(function(layer){layer.showLabel()});
