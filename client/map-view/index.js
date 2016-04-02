675f9e75bdf1d08afa547777ac117c388c70c8ae
Jose Ramirez <jcramireztello@gmail.com>
2016-04-01 20:38:11 -0500

    agregando leaflet_layergroup_collision.js

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -4,6 +4,7 @@ var plugins = require('./leaflet_plugins');
 var polyUtil = require('./polyline_encoded.js');
 var routeboxer = require('./leaflet_routeboxer.js');
 var leaflet_label = require('./leaflet_label/leaflet.label-src.js');
+var collision = require('./leaflet_layergroup_collision.js');
 var session = require('session');
 
 var center = config.geocode().center.split(',').map(parseFloat)
