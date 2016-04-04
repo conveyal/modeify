d215588736b9fc82beaf7cf9b72124ba394c9a7f
Jose Ramirez <jcramireztello@gmail.com>
2016-04-04 17:28:43 -0500

    push

--- client/map-view/leaflet_layergroup_collision.js
+++ client/map-view/leaflet_layergroup_collision.js
@@ -636,14 +636,15 @@ function extensions(parentClass) { return {
 		*/
 
 		for (var i=0; i< layer.length;i++) {
-            //this._staticLayers.push(layer[i]);
-            //parentClass.prototype.addLayer.call(this, layer[i]);
-
-            this._originalLayers.push(layer[i]);
+            this._staticLayers.push(layer[i]);
+            parentClass.prototype.addLayer.call(this, layer[i]);
 		}
 
         return;
 
+        this._staticLayers.push(layer);
+		parentClass.prototype.addLayer.call(this, layer);
+		return;
 
 		this._originalLayers.push(layer);
 		if (this._map) {
