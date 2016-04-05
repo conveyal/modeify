ba2be071d11cf6f4f1bc6eb2590a3a7e4e97c4d1
Jonathan quiza <jony327@gmail.com>
2016-04-05 01:18:15 -0500

    change html

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -217,13 +217,14 @@ module.exports.marker_map_point = function(to, map, set_hover){
 
     var name = to[2];
     var class_name;
+    var html;
     if (!set_hover){
-        class_name = 'leaflet-div-icon1'
+        class_name = 'leaflet-div-icon1';
+        html = "<div><span class='leaflet-label'>" + name + "</span></div>";
     }else{
-        class_name = 'leaflet-div-icon2'
+        class_name = 'leaflet-div-icon2';
+        html = "";
     }
-    console.log(class_name)
-    var html = "<div><span class='leaflet-label'>" + name + "</span></div>";
     var marker = L.marker({"lat":to[0], "lng": to[1]}, {
 				icon: L.divIcon({
 					html:html,
