03da3772eab0e2f1d3c1e00d813b23d7f24f3d6e
jonathan <Furystrikee123>
2016-04-05 16:47:22 -0500

    changes styles

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -319,7 +319,7 @@ module.exports.drawRouteAmigo = function(legs,mode, option) {
        }
        if (option.class_name){
             //color_options.className = 'message_';
-           color_options.className = 'fade-out four';
+           color_options.className = 'fadeInx';
             color_options.color = '#EFF1F2';
        }
 
