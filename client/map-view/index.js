6a281554b137f978446fb119c9c07aceb7704a68
jonathan <Furystrikee123>
2016-04-04 19:46:25 -0500

    adding changes css

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -308,7 +308,7 @@ module.exports.drawRouteAmigo = function(legs,mode, option) {
        }
        if (option.class_name){
             color_options.className = 'message_';
-            color_options.color = '#7FFFD4';
+            color_options.color = '#000';
        }
         console.log("color_options", color_options);
        //stroke: false;
