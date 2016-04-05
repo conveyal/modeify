f302cab20e2bae31bf4f638dd8644ba1041c7945
jonathan <Furystrikee123>
2016-04-04 19:26:00 -0500

    adding changes css

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -303,9 +303,9 @@ module.exports.drawRouteAmigo = function(legs,mode, option) {
             dashArray: dasharray
         };
 
-       //if (option.stroke){
-       //  delete color_options.opacity;
-       //}
+       if (option.stroke){
+         delete color_options.opacity;
+       }
        if (option.class_name){
             //color_options.className = 'message_';
             color_options.color = '#7FFFD4';
