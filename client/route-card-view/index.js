a422b04ef5465dc50730458c140a04872f1e8ff1
jonathan <Furystrikee123>
2016-04-06 14:41:50 -0500

    adding changes

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -28,6 +28,7 @@ var View = module.exports = view(require('./template.html'), function(view, mode
                 rec.attr('class', 'iteration-'+i+' legend-fadeout');
 
                var rec2 = d3.selectAll(".leaflet-marker-pane").filter(".leaflet-div-icon1 circle-fade-"+i);
+               console.log(rec2)
                rec2.attr('class', 'leaflet-marker-icon leaflet-div-icon2 circle-fade-'+i+ ' leaflet-zoom-hide');
            }
       }
