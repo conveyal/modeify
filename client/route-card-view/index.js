76ece2d1413b7a1e2f7d3f220cb831df644e9cb5
Jose Ramirez <jcramireztello@gmail.com>
2016-04-06 10:13:01 -0500

    color

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -27,7 +27,7 @@ var View = module.exports = view(require('./template.html'), function(view, mode
            if (i!=model.index){
                 d3.selectAll(".iteration-"+i)
                 .attr("color", "#000000")
-                .transition().duration(400).style("stroke", "#E0E0E0");
+                .transition().duration(500).style("stroke", "#E0E0E0");
 
                 //.transition().duration(400).classed("hide-route", true);
                 /*
