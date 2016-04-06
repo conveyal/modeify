689826c9e6b4da9509019fd3c116e902b316ae35
Jonathan quiza <jony327@gmail.com>
2016-04-06 01:58:17 -0500

    remomve style bold

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -27,6 +27,7 @@ var View = module.exports = view(require('./template.html'), function(view, mode
            if (i!=model.index){
                 var rec = d3.selectAll(".iteration-"+i);
                 rec.attr('class', 'iteration-'+i+' legend-fadeout');
+
                 //rec.transition().duration(400).style("stroke", "#E0E0E0");
                 //.style("opacity", 1)
                 //.transition().duration(500).style("opacity", 0);
