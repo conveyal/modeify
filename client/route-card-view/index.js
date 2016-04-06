87f71c5976668c67bf15a28c514f67f8faf95daf
jonathan <Furystrikee123>
2016-04-06 14:23:06 -0500

    adding styles hiden

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -27,8 +27,8 @@ var View = module.exports = view(require('./template.html'), function(view, mode
                 var rec = d3.selectAll(".iteration-"+i);
                 rec.attr('class', 'iteration-'+i+' legend-fadeout');
 
-               var rec2 = d3.selectAll(".leaflet-div-icon1 .iteration-"+i);
-               rec2.attr('class', 'leaflet-marker-icon leaflet-div-icon2 leaflet-zoom-hide');
+               var rec2 = d3.selectAll(".leaflet-div-icon1 .circle-fade-"+i);
+               rec2.attr('class', 'leaflet-marker-icon leaflet-div-icon2 iteration-'+i+' leaflet-zoom-hide');
            }
       }
 
@@ -43,8 +43,8 @@ var View = module.exports = view(require('./template.html'), function(view, mode
              var rec = d3.selectAll(".iteration-"+i);
              rec.attr('class', 'iteration-'+i);
 
-             var rec2 = d3.selectAll(".leaflet-div-icon2 .iteration-"+i);
-             rec2.attr('class', 'leaflet-marker-icon leaflet-div-icon1 leaflet-zoom-hide');
+             var rec2 = d3.selectAll(".leaflet-div-icon2 .circle-fade-"+i);
+             rec2.attr('class', 'leaflet-marker-icon leaflet-div-icon1 iteration-'+i+' leaflet-zoom-hide');
         }
    }
 
