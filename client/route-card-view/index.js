abd2463758c12604cae75f9eb075fe2870eb9ab6
Jose Ramirez <jcramireztello@gmail.com>
2016-04-06 14:45:10 -0500

    insert before

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -51,7 +51,7 @@ var View = module.exports = view(require('./template.html'), function(view, mode
 
       d3.selectAll(".estaes")[0].sort(function(a,b){
             if (d3.select(a).attr("color") == "#000000") {
-                d3.select(a).node().parentNode.appendChild(a);
+                d3.select(a).node().parentNode.insertBefore(a, d3.select(".estaes"));
                 console.log("si agrega");
             }else{
                 console.log("esta es a ->", a);
