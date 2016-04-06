0fc1de5eba9b1c5f635e1b2cae07eb14b0a9071f
jonathan <Furystrikee123>
2016-04-06 14:10:08 -0500

    adding styles hiden

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -27,7 +27,7 @@ var View = module.exports = view(require('./template.html'), function(view, mode
                 var rec = d3.selectAll(".iteration-"+i);
                 rec.attr('class', 'iteration-'+i+' legend-fadeout');
 
-               var rec2 = d3.selectAll(".leaflet-div-icon1");
+               var rec2 = d3.selectAll(".leaflet-div-icon1 .iteration-"+i);
                rec2.attr('class', 'leaflet-marker-icon leaflet-div-icon2 leaflet-zoom-hide legend-fadeout');
            }
       }
