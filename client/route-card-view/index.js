780f102be7b8353708649541d292c8c7eca11408
Jose Ramirez <jcramireztello@gmail.com>
2016-04-06 09:54:49 -0500

    update

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -26,7 +26,7 @@ var View = module.exports = view(require('./template.html'), function(view, mode
       for (var i=0; i<itineration.length;i++) {
            if (i!=model.index){
                 d3.selectAll(".iteration-"+i)
-                .attr("color", "#0000")
+                .attr("color", "#000000")
                 .transition().duration(400).style("stroke", "#E0E0E0");
 
                 //.transition().duration(400).classed("hide-route", true);
@@ -77,7 +77,8 @@ var View = module.exports = view(require('./template.html'), function(view, mode
         if (i!=model.index){
 
         d3.selectAll(".iteration-"+i)
-        .attr('stroke', function(d){
+        .transition().duration(500)
+        .style('stroke', function(d){
                 console.log(this);
                 return this.color;
             });
