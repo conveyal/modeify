e6a1faf009fa9adf09b354e0612f67c109007444
Jose Ramirez <jcramireztello@gmail.com>
2016-04-05 20:12:22 -0500

    d stroke

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -25,9 +25,13 @@ var View = module.exports = view(require('./template.html'), function(view, mode
       console.log("obj itineration 1 ->", itineration.length);
       for (var i=0; i<itineration.length;i++) {
            if (i!=model.index){
-                d3.selectAll(".iteration-"+i)
-                .style("opacity", 1)
-                .transition().duration(500).style("opacity", 0);
+                var rec = d3.selectAll(".iteration-"+i);
+                rec.attr('data-color', function(d){
+                    console.log("stroke ->" , d.stroke);
+                    return d.stroke;
+                }).transition().duration(400).style("stroke", "#E0E0E0");
+                //.style("opacity", 1)
+                //.transition().duration(500).style("opacity", 0);
                //.transition().duration(400).style("stroke", "#E0E0E0");
                //.style("opacity", 1)
            }
