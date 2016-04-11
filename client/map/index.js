9ae570fd55ab47a8a394beb512417cb8f4bd9dac
jonathan <Furystrikee123>
2016-04-11 18:46:15 -0500

    adding comment map view

--- client/map/index.js
+++ client/map/index.js
@@ -356,15 +356,8 @@ module.exports.addPoint = function (map, point) {
 	var zoomHideEl = document.querySelectorAll('path.realtimemarker');
 	console.log("zoomHideEl", zoomHideEl);
 	for (i in zoomHideEl) {
-	    console.log(zoomHideEl[i]);
-
 	    var parent = zoomHideEl[i].parentNode;
-	    console.log("parent ->", parent);
-
-	    //parent.style.display = 'none';
 	}
-	//var zoomHideEl = document.querySelectorAll('svg.leaflet-zoom-hide')[0];
-	//if (zoomHideEl) zoomHideEl.style.display = 'none';
 	module.exports.deleteRoute(this);
     });
 
