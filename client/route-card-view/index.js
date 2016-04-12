18aad29b772b914350279cbb6528e6b30dd33645
Jose Ramirez <jcramireztello@gmail.com>
2016-04-12 09:44:00 -0500

    ordena luego agrega

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -72,12 +72,15 @@ var View = module.exports = view(require('./template.html'), function(view, mode
             var element = d3.select(layer_ordenados[i]);
             var child = element.select("path");
             element.attr("data-show", "0");
+
+            element.node().parentNode.appendChild(layer_ordenados[i]);
+
             child.transition().duration(600).style("stroke",function(i,v){
                     return d3.select(this).attr("stroke");
 
             });
             child.attr("data-show", "0");
-            element.node().parentNode.appendChild(layer_ordenados[i]);
+
         }
     /*
    showMapView.cleanPolyline();
