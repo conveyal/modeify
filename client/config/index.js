48727dfcfc090c5e2c54b5c7c579ac6ec885ff1b
Trevor Gerhardt <trevorgerhardt@gmail.com>
2016-05-26 12:37:15 -0500

    No need to merge in config file client side

--- client/config/index.js
+++ client/config/index.js
@@ -1,13 +1,7 @@
 var each = require('component-each')
 
 var config = JSON.parse(process.env.SETTINGS)
-
-var env = process.env.NODE_ENV
-
-config.env = env
-for (var key in config.environments[env]) {
-  config[key] = config.environments[env][key] || config[key] || ''
-}
+config.env = process.env.NODE_ENV
 
 each(config, function (key, val) {
   module.exports[key] = module.exports[key.toLowerCase()] = function () {
