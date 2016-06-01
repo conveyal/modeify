69ececc02e936d79008717b631aa68a07b8aa3a3
David Emory <demory@conveyal.com>
2016-06-01 12:44:50 -0400

    Update commuter form copy

--- client/commuter-form/index.js
+++ client/commuter-form/index.js
@@ -117,7 +117,7 @@ View.prototype.save = function (e) {
   // set the rest of the data
   this.model.set(data)
 
-  var text = this.model.isNew() ? 'Added & invited new commuter.' : 'Saved changes to commuter.'
+  var text = this.model.isNew() ? 'Added new commuter.' : 'Saved changes to commuter.'
 
   var self = this
   this.model.save(function (err) {
