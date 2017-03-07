7b9109bd19a28ae2bb9dd6963455ded86ff22d47
Evan Siroky <evan.siroky@yahoo.com>
2017-03-07 14:19:27 -0800

    test(server > r5): convert test into jest test

---
+++ __tests__/lib/r5.js
@@ -0,0 +1,47 @@
+/* globals describe, expect, it */
+
+process.env.TEST_R5_URL = 'http://mock-host.com'
+
+import nock from 'nock'
+
+const r5 = require('../../lib/r5')
+
+describe('r5', () => {
+  it('should request plan', async () => {
+    nock('http://mock-host.com')
+      .post('/')
+      .reply(200, (uri, requestBody) => {
+        expect(uri).toBe('/')
+        expect(requestBody.query).toMatchSnapshot()
+        expect(JSON.parse(requestBody.variables)).toMatchSnapshot()
+        return {
+          data: {
+            plan: {
+              fake: 'response'
+            }
+          }
+        }
+      })
+
+    const response = await r5.requestPlan({
+      from: {
+        lon: -77.029199,
+        lat: 38.943037
+      },
+      to: {
+        lon: -77.085016,
+        lat: 38.891254
+      },
+      date: '2016-06-17',
+      fromTime: '7:00',
+      toTime: '9:00',
+      accessModes: 'WALK,BICYCLE,BICYCLE_RENT,CAR_PARK',
+      directModes: 'CAR,WALK,BICYCLE,BICYCLE_RENT',
+      egressModes: 'WALK',
+      transitModes: 'TRAINISH'
+    })
+
+    delete response.responseTime
+    expect(response).toMatchSnapshot()
+  })
+})
