ba903b32a64d807e9ceea404f15f4ec0288afeb5
David Emory <demory@conveyal.com>
2017-12-06 20:18:32 -0500

    feat(alerts): Add support for custom service alert header content (specified in config)

--- client/service-alerts-view/index.js
+++ client/service-alerts-view/index.js
@@ -1,6 +1,7 @@
 var view = require('../view')
 var hogan = require('hogan.js')
 var each = require('component-each')
+var config = require('../config')
 
 var alertTemplate = hogan.compile(require('./alert.html'))
 
@@ -14,8 +15,16 @@ var View = module.exports = view(require('./template.html'), function (view, mod
   })
 })
 
+View.prototype.hasHeader = function () {
+  return typeof config.service_alerts === 'function' && config.service_alerts().header
+}
+
 View.prototype.hasAlerts = function () {
   return this.model.serviceAlerts && this.model.serviceAlerts.length > 0
+}
+
+View.prototype.header = function () {
+  return this.hasHeader() ? config.service_alerts().header : ''
 }
 
 View.prototype.alerts = function () {
